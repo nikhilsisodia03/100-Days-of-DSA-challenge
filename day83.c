bcba71231e421532d32afad51dee62f5866647a7
Nikhil Sisodia <Nikhil.25071@stu.upes.ac.in>
2026-04-24 12:53:15 +0530

    Implement selection sort algorithm in C

---
+++ day83.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    selectionSort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d%s", arr[i], (i == n - 1) ? "" : " ");
+    }
+    printf("\n");
+
+    return 0;
+}
